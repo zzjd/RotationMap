5c92f7be66cb8e0e5e943dfb0c7c1fe708cef746
yizhiran <1872600241@qq.com>
2020-12-27 15:38:12 +0800

    feat: 使用hooks改写轮播图

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,89 @@
-/* eslint-disable radix */
-/* eslint-disable camelcase */
-/* eslint-disable no-loop-func */
-/* eslint-disable func-names */
-/* eslint-disable no-plusplus */
-/* eslint-disable jsx-a11y/anchor-has-content */
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import { Button } from 'antd';
 
 const App = () => {
-  const [datas, setDatas] = useState([]);
+  const [imgsArray, setImgsArray] = useState([]);
+  const [timer, setTimer] = useState(null);
+  const [curIndex, setCurIndex] = useState(0);
+  const [curImg, setCurImg] = useState(null);
 
   const handleImgsChange = async () => {
+    // 解决“如果curIndex为4，而新选择的图片小于4，就会出错”的问题
+    setCurIndex(0);
     const imgs = document.getElementById('selectImgs').files;
     const tempDatas = [];
-    const imgsArray = Object.values(imgs);
-    for (let i = 0; i < imgsArray.length; i++) {
+    for (let i = 0; i < Object.values(imgs).length; i++) {
       const reader = new FileReader();
-      reader.readAsDataURL(imgsArray[i]);
+      reader.readAsDataURL(Object.values(imgs)[i]);
       reader.onload = function (e) {
-        // console.log(e.target.result, 'e.target.result');
         tempDatas.push(e.target.result);
+        setImgsArray(tempDatas);
       };
-      setDatas(tempDatas);
     }
   };
+
+  const display = () => {
+    if (imgsArray.length === 0 || curIndex === imgsArray.length - 1) {
+      setCurIndex(0);
+    } else {
+      setCurIndex(curIndex + 1);
+    }
+  };
+
+  const nextPlay = () => {
+    if (imgsArray.length === 0 || curIndex === imgsArray.length - 1) {
+      setCurIndex(0);
+    } else {
+      setCurIndex(curIndex + 1);
+    }
+  };
+
+  const lastPlay = () => {
+    if (curIndex === 0) {
+      setCurIndex(imgsArray.length - 1);
+    } else {
+      setCurIndex(curIndex - 1);
+    }
+  };
+
+  useEffect(() => {
+    if (imgsArray.length !== 0) {
+      clearTimeout(timer);
+      setTimer(setTimeout(display, 1500));
+      setCurImg(imgsArray[curIndex]);
+    }
+  }, [curIndex, imgsArray]);
 
   return (
     <div className="App">
       <input type="file" id="selectImgs" webkitdirectory multiple onChange={handleImgsChange} />
-      <div className="wrapper">
-        <div className="box">
-          <ul className="imgbox">
-            <li className="imgshow">
-              <img src="images/1.jpg" alt="" />
-            </li>
-          </ul>
+      {imgsArray.length === 0 ? null : (
+        <div className="wrapper">
+          <div className="box">
+            <ul className="imgbox">
+              <li className="imgshow">
+                <img src={curImg} alt="" />
+              </li>
+            </ul>
+          </div>
+          <div className="next" onClick={nextPlay} />
+          <div className="last" onClick={lastPlay} />
+          <div className="dot" id="dot">
+            <ul>
+              {imgsArray.map((item, index) => (
+                <li
+                  key={item}
+                  className={curIndex === index ? 'act' : ''}
+                  style={{ listStyleType: 'none' }}
+                  onClick={() => setCurIndex(index)}
+                >
+                  <a href="#" />
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-        <div className="next" />
-        <div className="last" />
-        <div className="dot">
-          <ul>
-            <li style={{ listStyleType: 'none' }} />
-            <li style={{ listStyleType: 'none' }} />
-            <li style={{ listStyleType: 'none' }} />
-            <li style={{ listStyleType: 'none' }} />
-          </ul>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
