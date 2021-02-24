19a616f775041e252de01dfecf77be32bfb9d91f
zhouzhijie <1872600241@qq.com>
2021-02-24 17:35:53 +0800

    fix: 规范代码

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,58 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
 
 const App = () => {
-  const [imgsArray, setImgsArray] = useState([]);
-  const [timer, setTimer] = useState(null);
-  const [curIndex, setCurIndex] = useState(0);
-  const [curImg, setCurImg] = useState(null);
+  const [imgsArray, setImgsArray] = useState([])
+  const [timer, setTimer] = useState(null)
+  const [curIndex, setCurIndex] = useState(0)
+  const [curImg, setCurImg] = useState(null)
 
   const handleImgsChange = async () => {
     // 解决“如果curIndex为4，而新选择的图片小于4，就会出错”的问题
-    setCurIndex(0);
-    const imgs = document.getElementById('selectImgs').files;
-    const tempDatas = [];
+    setCurIndex(0)
+    const imgs = document.getElementById('selectImgs').files
+    const tempDatas = []
     for (let i = 0; i < Object.values(imgs).length; i++) {
-      const reader = new FileReader();
-      reader.readAsDataURL(Object.values(imgs)[i]);
+      const reader = new FileReader()
+      reader.readAsDataURL(Object.values(imgs)[i])
       reader.onload = function (e) {
-        tempDatas.push(e.target.result);
-        setImgsArray(tempDatas);
-      };
+        tempDatas.push(e.target.result)
+        setImgsArray(tempDatas)
+      }
     }
-  };
+  }
 
   const display = () => {
     if (imgsArray.length === 0 || curIndex === imgsArray.length - 1) {
-      setCurIndex(0);
+      setCurIndex(0)
     } else {
-      setCurIndex(curIndex + 1);
+      setCurIndex(curIndex + 1)
     }
-  };
+  }
 
   const nextPlay = () => {
     if (imgsArray.length === 0 || curIndex === imgsArray.length - 1) {
-      setCurIndex(0);
+      setCurIndex(0)
     } else {
-      setCurIndex(curIndex + 1);
+      setCurIndex(curIndex + 1)
     }
-  };
+  }
 
   const lastPlay = () => {
     if (curIndex === 0) {
-      setCurIndex(imgsArray.length - 1);
+      setCurIndex(imgsArray.length - 1)
     } else {
-      setCurIndex(curIndex - 1);
+      setCurIndex(curIndex - 1)
     }
-  };
+  }
 
   useEffect(() => {
     if (imgsArray.length !== 0) {
-      clearTimeout(timer);
-      setTimer(setTimeout(display, 1500));
-      setCurImg(imgsArray[curIndex]);
+      clearTimeout(timer)
+      setTimer(setTimeout(display, 1500))
+      setCurImg(imgsArray[curIndex])
     }
-  }, [curIndex, imgsArray]);
+  }, [curIndex, imgsArray])
 
   return (
     <div className="App">
@@ -85,7 +85,7 @@ const App = () => {
         </div>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
